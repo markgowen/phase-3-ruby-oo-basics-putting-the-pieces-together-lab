86ca309d332d5f5faf416c01d71a3620295aa957
Mark <mark.gowen78@gmail.com>
2022-12-13 15:07:45 -0800

    Book solution

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre, :turn_page
+  attr_reader :turn_page
 
 
+  def initialize(title)
+    @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
